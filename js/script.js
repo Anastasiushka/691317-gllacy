5993faa84e4dd8f7b1308bb5ef9752a9aa2abae9
Anastasiushka <39620434+Anastasiushka@users.noreply.github.com>
2018-06-27 22:34:45 +0300

    Осталось 2 вопроса по JS

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 var link = document.querySelector(".button-location");
 var feedback = document.querySelector(".modal-feedback-form");
-var body = document.querySelector("body");
+var container = document.querySelector(".feedback-container");
 var close = feedback.querySelector(".modal-close");
 var form = feedback.querySelector("form");
 var name = feedback.querySelector("#name-feedback");
 var email = feedback.querySelector("#email-feedback");
 var message = feedback.querySelector("#message-feedback");
 
+
 link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    feedback.classList.add("modal-show");
-    body::after.style.display = "block";                     /*  Как сделать оверлей? */
+    evt.preventDefault();                 
+    container.classList.add("modal-show");
     name.focus();                                       /* Не работает фокус */
 });
 
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
-    feedback.classList.remove("modal-show");
+    container.classList.remove("modal-show");
     feedback.classList.remove("modal-error");
 });
 
@@ -32,11 +32,9 @@ form.addEventListener("submit", function(evt) {
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
         evt.preventDefault();
-        if (feedback.classList.contains("modal-show")) {
-            feedback.classList.remove("modal-show");
+        if (container.classList.contains("modal-show")) {
+            container.classList.remove("modal-show");
             feedback.classList.remove("modal-error");
         }
     }
 });
-
-/* Как сделать карту? Сделать ли для нее дополнительный попап? Или она должна лежать прямо на странице? */
